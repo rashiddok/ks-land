f5f72fb9e93cad5f51aa825ad2b612842afd3a0d
escettitt <tekillermd@gmail.com>
2022-04-13 20:50:30 +0300

    try

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ git pull
 
 npm install
 npm run build:prod
+npm run build:en
 
 echo "Copy app htaccess..."
 cp .htaccess.app .htaccess
@@ -12,3 +13,7 @@ cp .htaccess.dist dist/ks-land/.htaccess
 
 echo "Moving to public_html..."
 cp -a dist/ks-land/. ../public_html/
+
+echo "Moving en to public_html..."
+mkdir ../public_html/en
+cp -a dist/ks-land-en/en ../public_html/en
