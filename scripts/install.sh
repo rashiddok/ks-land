e2bad29d44795fb15ed00fe3026438b4028667fe
escettitt <tekillermd@gmail.com>
2022-04-13 15:23:33 +0300

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ npm run build:prod
 echo "Copy app htaccess..."
 cp .htaccess.app .htaccess
 echo "Copy dist htaccess..."
-cp .htaccess.dist dist/ks-land
+cp .htaccess.dist dist/ks-land/.htaccess
 
 echo "Moving to public_html..."
-cp dist/ks-land ../public_html
+cp -a dist/ks-land/. ../public_html/
