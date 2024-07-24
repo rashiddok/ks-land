888a8009c2d79c7c868784340de3acd16d2a4a60
rashiddok <maksim.rasheuski@itechart-group.com>
2024-07-24 13:31:17 +0300

    v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,28 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LayoutModule } from './layout/layout.module';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {ProjectsStore} from "./shared/projects.store";
 import {ProjectsService} from "./shared/projects.service";
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    LayoutModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
-  ],
-  providers: [
-    {
-      provide: ProjectsStore,
-      deps: [ProjectsService]
-    },
-    {
-      provide: ProjectsService
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        LayoutModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            registrationStrategy: 'registerWhenStable:30000'
+        })], providers: [
+        {
+            provide: ProjectsStore,
+            deps: [ProjectsService]
+        },
+        {
+            provide: ProjectsService
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
